44ae68ec5b5d95a12c8a3648b1fd58e8151eac01
patrick nsukami <ndkpatt@gmail.com>
2014-12-05 03:51:51 +0000

    When deploying in production server, a list of allowed hosts should be set

--- wappa/settings/prod.py
+++ wappa/settings/prod.py
@@ -6,3 +6,6 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 TEMPLATE_DEBUG = False
+
+# You must set ALLOWED_HOSTS if Debug is false
+ALLOWED_HOSTS = ['178.62.127.105'] # ['*'] or ['127.0.0.1', 'localhost'] or [www.wappa.com]
