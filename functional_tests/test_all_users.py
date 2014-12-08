82d785d85aadb2a6fab4be26c9a028bb17e4a7ac
patrick nsukami <ndkpatt@gmail.com>
2014-12-08 16:19:01 +0000

    Workflow: the development will be done in the develop branch
    The development process will be done in the develop branch.
    We will try to keep a clean master branch.
    
    First fonctional test added, but not yet passed.
    This functional test will be used as an example for
    futur functional tests that should be written.
    
    Breaking changes with description of the change, justification and migration notes
    Closes|Refs #

--- functional_tests/test_all_users.py
+++ functional_tests/test_all_users.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
+
 import unittest
 
+from selenium.webdriver.firefox import webdriver
+from django.core.urlresolvers import reverse
+from django.contrib.staticfiles.testing import LiveServerTestCase
 
 class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
-        self.browser = webdriver.Firefox()
+        self.browser = webdriver.WebDriver()
         self.browser.implicitly_wait(3)
 
     def tearDown(self):
@@ -15,6 +18,29 @@ class NewVisitorTest(unittest.TestCase):
     def test_it_worked(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('Welcome to Django', self.browser.title)
+
+
+class HomeNewVisitor(LiveServerTestCase):
+    def setUp(self):
+        self.browser = webdriver.WebDriver()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def get_full_url(self, namespace):
+        return self.live_server_url + reverse(namespace)
+
+    def test_home_title(self):
+        self.browser.get(self.get_full_url("home"))
+        self.assertIn("Wappa", self.browser.title)
+
+    def test_h1_css(self):
+        self.browser.get(self.get_full_url("home"))
+        h1 = self.browser.find_element_by_tag_name("h1")
+        self.assertEqual(h1.value_of_css_property("color"),
+                         "rgba(200, 50, 255, 1)")
+
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
