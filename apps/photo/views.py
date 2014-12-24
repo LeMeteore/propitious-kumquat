e1d6c5e35e2718dc1bd2adb122dc27dfe8d9bb35
patrick nsukami <ndkpatt@gmail.com>
2014-12-24 17:49:09 +0000

    Serve humans.txt and robots.txt.
    Addition of url pattern and view to serve humans.txt and robots.txt.
    Modification (a little) of humans.txt & robots.txt.

--- apps/photo/views.py
+++ apps/photo/views.py
@@ -4,3 +4,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home_files(request, filename):
+    return render(request,
+                  filename,
+                  {},
+                  content_type="text/plain")
--- wappa/urls.py
+++ wappa/urls.py
@@ -3,11 +3,15 @@
 
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from apps.photo.views import home, home_files
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'wappa.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^(?P<filename>(robots.txt)|(humans.txt))$',
+        home_files, name='home-files'),
+)
 
     url(r'^admin/', include(admin.site.urls)),
 )
