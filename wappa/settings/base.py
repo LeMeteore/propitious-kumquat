8c55b1732f0d07fd818936783c89395ba14f5392
patrick nsukami <ndkpatt@gmail.com>
2014-12-26 00:08:14 +0000

    Internatinalization and localization:
    Installation
    - pytz package for managing timezone calculation
    
    Modification of base.txt requirements
    - install pytz in base configuration
    
    Modification of base.py
    - use timezone: true
    - timezone default: africa/dakar
    
    Refs Issue #2

--- wappa/settings/base.py
+++ wappa/settings/base.py
@@ -111,6 +111,9 @@ USE_L10N = True
 
 USE_TZ = True
 
+# https://docs.djangoproject.com/en/1.7/topics/i18n/timezones/
+TIME_ZONE = 'Africa/Dakar'
+
 # languages that we want to use for translations
 # http://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
 LANGUAGES = (
