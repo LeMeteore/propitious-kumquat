da8a282c496d7c5a886dcfc4aa39c7438f71e147
patrick nsukami <ndkpatt@gmail.com>
2015-02-02 04:38:52 +0000

    Photo: update of models.py inside photo app.
    Modification of Photo application:
    
    - use types from Taxonomy application.
    - remove Country class
    
    Modification of photo/admin.py:
    
    - no more stacked inline model.
    - no customization.

--- apps/photo/admin.py
+++ apps/photo/admin.py
@@ -8,51 +8,14 @@ from hvad.admin import TranslatableAdmin
 from django.utils.translation import ugettext_lazy as _
 
 
-class PhotoInline(admin.TabularInline):
-    model = Photo.packs.through
-
-class CountryAdmin(TranslatableAdmin):
-    pass
-    # use_fieldsets = (
-    #     (_("Common"), {
-    #         'fields': ('code',),
-    #         }),
-    #     (_("Language dependent"), {
-    #         'fields': ('name',),
-    #         }),
-    #     )
-
-    # def get_fieldsets(self, request, obj=None):
-    #     return self.use_fieldsets
-
 class PhotoModelAdmin(admin.ModelAdmin):
+
     def save_model(self, request, obj, form, change):
         obj.save()
         UploadToAS3.delay(obj.image.name)
 
 class PackModelAdmin(TranslatableAdmin):
-    inlines = [
-        PhotoInline,
-    ]
-    # what's displayed in the change/read list
-    #list_display = ('get_countries',)
-    # def get_countries(self, obj):
-    #     return "\n".join([c.lazy_translation_getter('name') for c in obj.countries.all()])
-
-    # use_fieldsets = (
-    #     (_("Common"), {
-    #         'fields': ('domain', 'image',
-    #                    'date published', 'pack_type', 'status', 'tags',
-    #                    'begin_date', 'end_date')
-    #         }),
-    #     (_("Language dependent"), {
-    #         'fields': ('title', 'description'),
-    #         }),
-    #     )
-
-    # def get_fieldsets(self, request, obj=None):
-    #     return self.use_fieldsets
+    pass
 
 admin.site.register(Photo, PhotoModelAdmin)
 admin.site.register(Pack, PackModelAdmin)
-admin.site.register(Country, CountryAdmin)
--- apps/photo/models.py
+++ apps/photo/models.py
@@ -5,38 +5,20 @@ from django.db import models
 from datetime import datetime
 from hvad.models import TranslatableModel, TranslatedFields
 from taggit.managers import TaggableManager
-from apps.photo import choices
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
-
-class Country(TranslatableModel):
-    code = models.CharField(max_length=2, primary_key=True)
-    translations = TranslatedFields(
-        name = models.CharField(max_length=200)
-        )
-
-    def __str__(self):
-        return "%s" % self.lazy_translation_getter('name', str(self.pk))
-
-    class Meta:
-        verbose_name = _("country")
-        verbose_name_plural = _("countries")
+from apps.taxonomy.models import Country, Domain, Status, Gender
 
 class Pack(TranslatableModel):
-    pack_type = models.CharField(max_length=200,
-                                choices=choices.PACK_TYPE_CHOICES,
-                                default=choices.ACTUALITE,
-                                verbose_name="type")
-    status = models.CharField(max_length=200,
-                              choices=choices.STATUS_CHOICES,
-                              default=choices.OFFLINE)
+    pack_type = models.ForeignKey(Gender, verbose_name=_('type'))
+    status = models.ForeignKey(Status, verbose_name=_('status'))
 
     translations = TranslatedFields(
         title = models.CharField(max_length=200),
         description = models.CharField(max_length=200)
         )
-    countries = models.ManyToManyField('Country')
-    domain = models.CharField(max_length=200)
+    countries = models.ManyToManyField(Country)
+    domain = models.ForeignKey(Domain, verbose_name=_('domain'))
     image = models.ImageField(max_length=200, upload_to='wappa')
     pub_date = models.DateField(default=datetime.now, name='date published')
     begin_date = models.DateField(default=datetime.now, verbose_name=_("begin_date"))
@@ -52,17 +34,15 @@ class Photo(models.Model):
     title = models.CharField(max_length=200, default='un titre de photo', verbose_name=_('title'))
     description = models.CharField(max_length=200, default='une description de photo',)
 
-    author = models.ForeignKey(User, default=1, verbose_name=_('author'))
+    author = models.ForeignKey(User, verbose_name=_('author'))
     license = models.CharField(max_length=200, default='bsd', verbose_name=_('license'))
 
     countries = models.ManyToManyField(Country, verbose_name=_('countries'))
     image = models.ImageField(max_length=200, upload_to='wappa')
     pub_date = models.DateField(name='date published', default=datetime.now)
     packs = models.ManyToManyField(Pack, related_name='images')
-    status = models.CharField(max_length=200,
-                              choices=choices.STATUS_CHOICES,
-                              default=choices.OFFLINE,
-                              verbose_name=_('status'))
+    status = models.ForeignKey(Status, verbose_name=_('status'))
+
     tags = TaggableManager()
 
     def __str__(self):
