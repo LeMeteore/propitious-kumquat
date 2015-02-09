f7d1ed932229e396a6ee37e74a434b3459542537
patrick nsukami <ndkpatt@gmail.com>
2015-02-09 02:32:01 +0000

    Pack: remove photo from pack.
    Addition of a flash message after removing photo from pack with success.

--- apps/photo/admin.py
+++ apps/photo/admin.py
@@ -11,6 +11,8 @@ from django.conf.urls import patterns, url
 from django.core import urlresolvers
 from django.http import HttpResponseRedirect
 
+from django.contrib import messages
+
 class PhotoModelAdmin(admin.ModelAdmin):
 
     def save_model(self, request, obj, form, change):
@@ -34,6 +36,7 @@ class PackModelAdmin(TranslatableAdmin):
         return super(PackModelAdmin, self).change_view(request, object_id, form_url, extra_context=extra_context)
 
     def remove_photo_from_pack(self, request, photo_id, pack_id):
+        messages.add_message(request, messages.SUCCESS, _('photo removed from pack with success.'))
         photo = Photo.objects.get(pk=photo_id)
         Pack.objects.get(pk=pack_id).photos.remove(photo)
         return HttpResponseRedirect(urlresolvers.reverse("admin:photo_pack_change",args=(pack_id,)))
