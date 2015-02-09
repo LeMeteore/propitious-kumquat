50502253ba7b8c1a8df5169ef96f22d5fe5be2a9
patrick nsukami <ndkpatt@gmail.com>
2015-02-09 04:10:09 +0000

    Photo: addition of exif fields.
    Addition of some fields to store exif metadatas.
    Also, modification of tag fields inside photo & pack to permit null values.

--- apps/photo/models.py
+++ apps/photo/models.py
@@ -13,18 +13,24 @@ from django.core import urlresolvers
 from django.contrib.contenttypes.models import ContentType
 
 class Photo(models.Model):
-    title = models.CharField(max_length=200, default='un titre de photo', verbose_name=_('title'))
-    description = models.CharField(max_length=200, default='une description de photo',)
+    title = models.CharField(max_length=200, verbose_name=_('title'))
+    description = models.TextField(max_length=200, verbose_name=_('description'))
 
     author = models.ForeignKey(User, verbose_name=_('author'))
-    license = models.CharField(max_length=200, default='bsd', verbose_name=_('license'))
-
+    license = models.CharField(max_length=200, verbose_name=_('license'))
+    width = models.IntegerField(max_length=200, verbose_name=_('width'))
+    height = models.IntegerField(max_length=200, verbose_name=_('heigth'))
+    camera_model = models.CharField(max_length=200, verbose_name=_('camera_model'))
+    sensibilite_iso = models.CharField(max_length=200, verbose_name=_('sensibilite_iso'))
+    focal = models.CharField(max_length=200, verbose_name=_('focal'))
+    ouverture = models.CharField(max_length=200, verbose_name=_('ouverture'))
+    temps_de_pause = models.CharField(max_length=200, verbose_name=_('temps_de_pause'))
     countries = models.ManyToManyField(Country, verbose_name=_('countries'))
     image = models.ImageField(max_length=200, upload_to='wappa')
     pub_date = models.DateField(name='date published', default=datetime.now)
     status = models.ForeignKey(Status, verbose_name=_('status'))
 
-    tags = TaggableManager()
+    photo_tags = TaggableManager(blank=True)
 
     def __str__(self):
         return "%s" % self.title
@@ -34,13 +40,12 @@ class Photo(models.Model):
         return urlresolvers.reverse("admin:%s_%s_change" % (content_type.app_label, content_type.model),args=(self.id,))
 
 class Pack(TranslatableModel):
+    translations = TranslatedFields(
+        title = models.CharField(max_length=200, verbose_name=_('title')),
+        description = models.TextField(max_length=200, verbose_name=_('description'))
+        )
     pack_type = models.ForeignKey(Gender, verbose_name=_('type'))
     status = models.ForeignKey(Status, verbose_name=_('status'))
-
-    translations = TranslatedFields(
-        title = models.CharField(max_length=200),
-        description = models.CharField(max_length=200)
-        )
     countries = models.ManyToManyField(Country)
     domain = models.ForeignKey(Domain, verbose_name=_('domain'))
     image = models.ForeignKey(Photo, null=True, blank=True, verbose_name=_('image preview'))
@@ -48,8 +53,10 @@ class Pack(TranslatableModel):
     pub_date = models.DateField(default=datetime.now, name='date published')
     begin_date = models.DateField(default=datetime.now, verbose_name=_("begin_date"))
     end_date = models.DateField(default=datetime.now, verbose_name=_("end_date"))
-    tags = TaggableManager()
+
     photos = models.ManyToManyField(Photo, null=True, blank=True, related_name='packs')
+
+    pack_tags = TaggableManager(blank=True)
 
     def __str__(self):
         return "%s" % self.lazy_translation_getter('title', str(self.pk))
