945488c8daf7aca697eb1be52bb8c284281c2e8e
patrick nsukami <ndkpatt@gmail.com>
2015-02-13 01:57:43 +0000

    Display of user uploaded pictures.
    Update of urls.py for being able to display user uploaded content.
    This configuration is only valid when DEBUG = True. (development mode)

--- wappa/urls.py
+++ wappa/urls.py
@@ -6,6 +6,9 @@ from django.contrib import admin
 from django.conf.urls.i18n import i18n_patterns
 from apps.photo.views import home, home_files
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'wappa.views.home', name='home'),
@@ -19,3 +22,6 @@ urlpatterns += i18n_patterns(
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', home, name='home'),
 )
+
+if settings.DEBUG == True:
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
