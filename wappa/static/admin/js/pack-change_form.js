dd9fa9e0ccaafc2a44685ad40f43cee258faecf2
Sacripant <thomas@sacripant.fr>
2015-03-27 11:43:53 +0000

    js admin : new pack-change_form.js

---
+++ wappa/static/admin/js/pack-change_form.js
@@ -0,0 +1,16 @@
+/*
+**	Wappa Pack change_form
+**	© sacripant.fr
+
+**	UI for add and remove photos to a Pack
+*/
+
+
+
+(function($) {
+	$(document).ready(function() {
+
+		
+
+    });
+})(django.jQuery);
