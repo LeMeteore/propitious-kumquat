8558ae21589f4a78aa4bde1e44078903d818199a
patrick nsukami <ndkpatt@gmail.com>
2015-04-08 04:43:09 +0000

    Pack model unit testing. Refs Issue#9

--- apps/photo/tests.py
+++ apps/photo/tests.py
@@ -10,24 +10,25 @@ from django.utils import timezone
 from django.core.urlresolvers import reverse
 
 # Create your tests here.
-class PhotoTest(TestCase):
+class MyTest(TestCase):
     def setUp(self):
-        u = User()
-        u.save()
-        s = Status()
-        s.save()
+        u = User.objects.create()
+        s = Status.objects.create()
+        t = Gender.objects.create()
+        d = Domain.objects.create()
         c = Country.objects.language('en').create(name='Senegal', code='SN')
-        c.save()
         self.user = u
         self.status = s
         self.country = c
+        self.domain = d
+        self.type = t
 
-    def create_photo(self):
-        # the photo to upload
+    def create_photo(self, pk):
         my_home = os.environ['HOME']
         filename = "Images/mandelbrot_shrodinger_cat.jpg"
         file_to_upload = os.path.join(my_home, filename)
         f = File(open(file_to_upload, 'rb'))
+        # the photo to upload
         p = Photo(title = "une photo de test",
                   description = "description d'une photo de test",
                   author = self.user,
@@ -40,14 +41,40 @@ class PhotoTest(TestCase):
                   ouverture = "ouverture de test",
                   temps_de_pause = "temps de pause de test",
                   status = self.status,
-                  id = 5
+                  id = pk
                   )
         p.image.save("mandelbrot_shrodinger_cat.jpg", f)
         # p should be saved first before adding m2m relations
         p.countries.add(self.country)
         return p
 
+
+
+class PhotoTest(MyTest):
     def test_photo_creation(self):
-        p = self.create_photo()
+        p = self.create_photo(pk=5)
         self.assertTrue(isinstance(p, Photo))
+        self.assertEqual(p.__str__(), p.title)
+
+
+class PackTest(MyTest):
+    def create_pack(self):
+        photo = self.create_photo(pk=8)
+        p = Pack.objects.language('en').create(title = "une pack de test",
+                                                description = "description d'une pack de test",
+                                                pack_type = self.type,
+                                                status = self.status,
+                                                domain = self.domain,
+                                                image = photo,
+                                                id = 5
+                  )
+
+        # p should be saved first before adding m2m relations
+        p.countries.add(self.country)
+        p.photos.add(photo)
+        return p
+
+    def test_pack_creation(self):
+        p = self.create_pack()
+        self.assertTrue(isinstance(p, Pack))
         self.assertEqual(p.__str__(), p.title)
