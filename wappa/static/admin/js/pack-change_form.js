6b42069f4e34a758e11890d53e81d6e7875fa77f
Sacripant <thomas@sacripant.fr>
2015-05-22 16:32:23 +0000

    JS pack-change_form : add remove tile event

--- wappa/static/admin/js/pack-change_form.js
+++ wappa/static/admin/js/pack-change_form.js
@@ -12,19 +12,55 @@
 */
 
 
+// * Ajax Call
+// * Mustache render
+// * Add result to target
+// * remove tile
+// * add new
+// * favorite one.
 
-(function($) {
+
+
+var packPhotos = (function($) {
 	'use strict';
 
+	var photosList = [];
+		// photosTiles = $();
+
+
+	var removePhoto = function(e){
+
+		var $this = $(e.target),
+			tile = $this.parents('.i--pack_photos'),
+			tileID = tile.data('id');
+
+		// console.log(tileID);
+
+		var idIndex = photosList.indexOf(tileID+'');
+		photosList.splice(idIndex, 1);	
+		tile.remove();
+
+		$('#id_photos').val(photosList.join(','));
+
+
+		// console.log(photosList);
+
+	};
 
 	// Ajoute les images au template
 	var render = function(template, data, target){
 		var renderer = Mustache.render(template, {photos : data} );
-		target.append(renderer);
+			
+			// var html = $.parseHTML(renderer);
+			// var newTiles = $(html).filter('.i--pack_photos');
+			// photosTiles = photosTiles.add(newTiles);
+			// target.append(newTiles);
+
+			target.append(renderer);
 	};
 
 	// Récupère les infos des images via AJAX
-	var imgsInfo = function(callback){
+	var getImgInfo = function(callback){
 		$.ajax({
 			url: '/static/admin/js/test.txt',
 			// url :'/en/admin/photo/photo/informations/' + photosval + '/',
@@ -43,21 +79,44 @@
 		var photosInput = $('#id_photos'),
 			photosval = photosInput.val(),
 			template = $('#i-photo-tpl').html(),
-			target = $('#l--pack_photos');
+			photosWrapper = $('#l--pack_photos');
 
+		if(photosval) photosList = photosval.split(',');
+
+		console.log(photosList);
+
+		// console.log(photosList);
+		// console.log(photosList.length);
+		
 		Mustache.parse(template);
 
 		// Charge les previews des photos exitantes
-		if (photosval) {
-			imgsInfo(function(data){
-				render(template, data, target);
+		if (photosList.length) {
+			getImgInfo(function(data){
+				
+				render(template, data, photosWrapper);
+
+				// stock input value in array
+				// photosTiles = $('.i--pack_photos');
+
+				// console.log( $('[data-action=remove]', data) );
+				// console.log( $(data) );
 			});
 		}
 
-		// Charge une nouvelle photo (via django)
-		// photosInput.change(function() {
-		// 	console.log("change");
-		// 	console.log(dRLP_newImg);
-		// });		
+
+		// remove tiles
+		photosWrapper.on('click', '[data-action=remove]', removePhoto );
+
+					// $('[data-action=remove]', html).click(function(e) {
+			// 	console.log(e);
+			// });
+
+		// $('[data-action=remove]', photosTiles).click(function() {
+		// 	console.log('remove tile');
+		// });
     });
+
+
+
 })(django.jQuery);
