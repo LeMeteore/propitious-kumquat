a82fdb56fd41f3e9a3b117e4458011c25736430c
patrick nsukami <ndkpatt@gmail.com>
2015-06-18 03:47:23 +0000

    Password mgt, views & urls. Refs Issues #26 #25.

--- apps/photo/dashboard_urls.py
+++ apps/photo/dashboard_urls.py
@@ -15,38 +15,44 @@ from . import dashboard_views as views
 
 urlpatterns = patterns(
     '',
-    url(r'^$', views.dashboard, name='dashboard_index'),
-    url(r'^login/$', views.login, name='dashboard_login'),
-    url(r'^logout/$', views.logout, name='dashboard_logout'),
+    url(r'^$', views.dashboard, name='index'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^logout/$', views.logout, name='logout'),
 )
 
 
 # password related stuff
 urlpatterns += patterns(
-    url(r'^password/done/$', password_reset_complete),
-
-    url(r'^password/reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$',
-        password_reset_confirm,
-        {'post_reset_redirect' : '/password/done/'}),
-
+    '',
     url(r'^password/reset/$', password_reset,
         {'template_name':'photo/registration/password_reset_form.html',
          'email_template_name': 'photo/registration/password_reset_email.html',
-        'subject_template_name': 'photo/registration/password_reset_subject.txt',},
-        name='dashboard_password_reset'),
+         'subject_template_name': 'photo/registration/password_reset_subject.txt',
+         'post_reset_redirect':'dashboard:password_reset_done',},
+        name='password_reset'),
+
+    url(r'^password/reset/done/$', password_reset_done,
+        {'template_name': 'photo/registration/password_reset_done.html',},
+        name='password_reset_done'),
+
+    url(r'^password/reset/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
+        password_reset_confirm,
+        {'template_name': 'photo/registration/password_reset_confirm.html',
+         'post_reset_redirect' : 'dashboard:password_done'},
+        name='password_reset_confirm'),
 
     url(r'^password/change/$', password_change,
         {'template_name': 'photo/registration/password_change_form.html',
-         'post_change_redirect': 'dashboard_password_change_done'},
-        name='dashboard_password_change'),
-
-    url(r'^password/reset/done/$',
-        password_reset_done,
-        name='dashboard_password_reset_done'),
+         'post_change_redirect': 'dashboard:password_change_done'},
+        name='password_change'),
 
     url(r'^password/change/done/$',
         password_change_done,
         {'template_name':'photo/registration/password_change_done.html',},
-        name='dashboard_password_change_done'),
+        name='password_change_done'),
+
+    url(r'^password/done/$', password_reset_complete,
+        {'template_name':'photo/registration/password_reset_complete.html'},
+        name='password_done'),
 
 )
