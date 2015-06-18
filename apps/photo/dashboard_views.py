c2c558a8f2e3cd235c8466b4e0dba87dabd12892
patrick nsukami <ndkpatt@gmail.com>
2015-06-18 05:17:36 +0000

    Views listing Pack & Photo models. Refs Issue #26.

--- apps/photo/dashboard_views.py
+++ apps/photo/dashboard_views.py
@@ -9,6 +9,10 @@ from django.views.decorators.csrf import csrf_protect
 from django.contrib.auth import login as auth_login, logout as auth_logout
 from django.contrib.auth.forms import AuthenticationForm
 
+from apps.photo.models import Pack, Photo
+from django.core.paginator import Paginator, EmptyPage
+from django.core.paginator import PageNotAnInteger
+
 @login_required(login_url='/dashboard/login/')
 def dashboard(request):
     #return HttpResponse("this is the dashboard")
@@ -30,3 +34,35 @@ def login(request, authentication_form=AuthenticationForm,):
 def logout(request, template_name="photo/registration/logged_out.html"):
     auth_logout(request)
     return TemplateResponse(request, template_name)
+
+
+@login_required(login_url='/dashboard/login/')
+def packs(request):
+    packs_list = [x for x in Pack.objects.all()]
+    paginator = Paginator(packs_list, 10) # 10 packs per page
+    page = request.GET.get('page')
+    try:
+        packs = paginator.page(page)
+    except PageNotAnInteger:
+        packs = paginator.page(1)
+    except EmptyPage:
+        packs = paginator.page(paginator.num_pages)
+    template = 'photo/pack/list.html'
+    return render(request, template,
+                  {'packs': packs})
+
+
+@login_required(login_url='/dashboard/login/')
+def photos(request):
+    photos_list = [x for x in Photo.objects.all()]
+    paginator = Paginator(photos_list, 10) # 10 photos per page
+    page = request.GET.get('page')
+    try:
+        photos = paginator.page(page)
+    except PageNotAnInteger:
+        photos = paginator.page(1)
+    except EmptyPage:
+        photos = paginator.page(paginator.num_pages)
+    template = 'photo/photo/list.html'
+    return render(request, template,
+                  {'photos': photos})
